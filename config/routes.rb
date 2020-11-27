d348b4547690169d0eacb3cbbe281f18ccb6e931
Tony Blum <tonyblum@me.com>
2020-11-27 02:52:49 -0800

    Devise log in to root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,29 @@
 Rails.application.routes.draw do
+
+  # root to: "/users/sign_in"
+
+#  devise_scope :user do
+#  root to: "devise/sessions#new"
+#end
+
+#  devise_for :users
+
+
+  devise_for :users, path: 'users'
+
+  devise_scope :user do
+    authenticated :user do
+      root 'projects#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
   resources :projects do
     resources :tasks
   end
-  devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-#devise_for :user
-root to: "projects#index"
-end
+end   #devise_for :user
