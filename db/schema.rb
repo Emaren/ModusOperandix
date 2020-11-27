15308e201a98110377c1d446fa9fc12ead7ad661
Tony Blum <tonyblum@me.com>
2020-11-27 09:21:19 -0800

    new heroku app

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_26_120600) do
+ActiveRecord::Schema.define(version: 2020_11_27_170655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,16 +22,6 @@ ActiveRecord::Schema.define(version: 2020_11_26_120600) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_projects_on_user_id"
-  end
-
-  create_table "tasks", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.string "status"
-    t.bigint "project_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -49,5 +39,4 @@ ActiveRecord::Schema.define(version: 2020_11_26_120600) do
   end
 
   add_foreign_key "projects", "users"
-  add_foreign_key "tasks", "projects"
 end
